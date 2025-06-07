681030f222361ffb955b815d193486780f0fd5b8
ForestSpirit <forestspirit@email.cz>
2025-06-07 11:30:51 +0300

    new-v17

---
+++ _next/static/dGq-ze2VP6zju4Tjttdio/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/432-b92422a8273312b8.js","static/chunks/pages/index-9e344274698730bb.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
